8c3b4b42cf0f147008175a4b437d3861c37af374
iraqman12 <83186793+iraqman12@users.noreply.github.com>
2021-09-28 19:58:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", "0"))
-    API_HASH = os.environ.get("API_HASH", "")
+    API_ID = int(os.environ.get("API_ID", "8634206"))
+    API_HASH = os.environ.get("API_HASH", "fc8f066ba30e9cc6b0da2af1561f2744")
     LOG_ID = int(os.environ.get("LOG_ID", "0"))
-    SESSION_STRING = os.environ.get("SESSION_STRING", "")
+    SESSION_STRING = os.environ.get("SESSION_STRING", "AgAM2GjROA6X9Cjdscn0pTJum7BdZmNZzmRw8XcGjWTVDUKTEBirCUeJCDTngEoKvQJBHyCSjKJYMpw80efiSatOXo6BWCRSKL9opGD1dk90LKFXtcVgzvMnDbJP9MkEvb7RykutSgSdBfOoGj_Z5Pvub9CuyGBB_12YB0_hKMU_zQym0iyALBWFVcQCYBQwT4TlOMm7JCMwQ1Yt34Xz66ksbl_o7gcaZJe6o5FePUlBsE4tMwQ-FlKSXXVO-ZGTmZeSSPDtqqn2e_KUoRLfjII9R9elcpkCE76JcNL3eH75VdFm0mVoY6PtvDLWZ7Eii8mDj2MxH1t2dZtO5lFGqgd7d6VUsQA")
 
 class Database:
     VIDEO_CALL = {}
